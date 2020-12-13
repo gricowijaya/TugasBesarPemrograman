82318eead491dafb158146ed864dfb9de3f2dfde
Monica Arysta <74805946+monicaarysta@users.noreply.github.com>
2020-12-13 19:45:58 +0700

    Update TugasBesarPemrograman.c
    Menambahkan fungsi wishlist terbaru

--- TugasBesarPemrograman.c
+++ TugasBesarPemrograman.c
@@ -592,3 +592,27 @@ akhir :
     }
     system ("clear");
 }
+
+//=======================================================================//
+//**         Fungsi Untuk Meminta User Menginputkan Target      ***//
+//=======================================================================//
+// Nama Fungsi    : wishlist                                             //
+// Input Argumen  : int hargaBarangDiinginkan dan char namaTarget        //
+// Output Argumen : -                                                    //
+// Deskripsi      : User diminta untuk menginputkan deskripsi target     //
+//                  dan kisaran harga barang yang diinginkan             //
+//                  Nanti deskripsi target dan harga barang diinginkan   //
+//                  akan disimpan dan digunakan kembali saat melakukan   //
+//                  proses pencapaian target terhadap uang yang          //
+//                  dimiliki oleh user .                                 //
+// Versi : 1.0                                      Rev. 0               //
+// Tgl   : 04-12-2020                               Tgl: 04-12-2020      //
+// Luh Putu Monica Arysta Putri Suastawan/2005551090                     //
+// Kelas A                                                               //
+//=======================================================================// 
+void wish_list(){
+        printf ("Deskripsi Barang Yang Dicari:");
+        scanf  ("%s",&namaTarget);
+        printf ("Kisaran Harga :");
+        scanf  ("%d",&hargaBarangDiinginkan);
+}
