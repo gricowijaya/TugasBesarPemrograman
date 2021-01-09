d2e86691d488dd50d9799616ff45f0403344c23b
Luh Putu Monica Arysta Putri Suastawan <74805946+monicaarysta@users.noreply.github.com>
2021-01-09 09:23:38 +0700

    Menambahkan header file dana darurat

--- danaDarurat.c
+++ danaDarurat.c
@@ -1,4 +1,8 @@
-#include<stdio.h>
+#include "structure.h"
+#include "function.h"
+#include "variable.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 //=======================================================================//
 //***********   Fungsi Untuk Menampilkan Dana Darurat    ****************//
@@ -17,16 +21,19 @@
 // Gede Rico Wijaya - 2005551091                                         //
 // Kelas A                                                               //
 //=======================================================================//
-int DanaDarurat (int saldo){
+int DanaDarurat (int *saldo){
 
     //Deklarasi variabel int dana untuk menyimpan nilai dari Dana yang harus dipersiapkan 
     //int dana menyimpan nilai dari perhitungan nilai saldo yang diinput oleh user
-    int dana = saldo * 0.3;
-        printf ("Dalam Manajemen Keungangan Anda harus diperlukan Dana Darurat\n");
-        printf ("Mengapa ?\nKarena agar finansial bisa siap dalam keadaan darurat,\n");
-        printf ("Untuk itu maka diperlukan Dana Darurat yang disarankan yaitu\n30 persen dari saldo yang dimilki\n");
-        printf ("Jadi Dana Darurat yang harus ditabung untuk saat ini adalah: %d\n", dana);
+    int dana = *saldo * 0.3;
+        printf ("\t\t\t\t||=====================================================================================================||");
+        printf ("\t\t\t\t|| Dalam Manajemen Keungangan Anda harus diperlukan Dana Darurat\n                                     ||");                                  
+        printf ("\t\t\t\t|| Mengapa ?\nKarena agar finansial bisa siap dalam keadaan darurat,\n                                 ||");
+        printf ("\t\t\t\t|| Untuk itu maka diperlukan Dana Darurat yang disarankan yaitu\n30 persen dari saldo yang dimilki\n   ||");
+        printf ("\t\t\t\t|| Jadi Dana Darurat yang harus ditabung untuk saat ini adalah: %d\n                                   ||", dana);
+        printf ("\t\t\t\t||=====================================================================================================||");
+
     //untuk mengubah nilai saldo jika user memilih fungsi DanaDarurat yang akan menjadi saldo = saldo - dana
-    saldo = saldo - dana ;
+    *saldo = *saldo - dana ;
     return dana;
 }
